879e80bebba79493e5ed0d987a7f5e74b505601e
mohammd <moha58dawo@gmail.com>
2025-09-03 23:47:30 +0330

    ad(دیتای های مربط به المان های سایت )
    ad( دیتای های مربط به المان های سایت  شامل موارد و دیتای های اوبیه )

---
+++ assets/js/data.js
@@ -0,0 +1,27 @@
+// داده‌های نمونه
+const DATA = [
+  {
+    id: 1,
+    type: "aircraft",
+    title: "رایت فلایر",
+    year: 1903,
+    cover: "assets/img/padafnd.jpg",
+    summary: "اولین هواپیمای موتوردار موفق توسط برادران رایت."
+  },
+  {
+    id: 2,
+    type: "aircraft",
+    title: "بوئینگ ۷۴۷",
+    year: 1969,
+    cover: "assets/img/f14.jpg",
+    summary: "ملکهٔ آسمان‌ها؛ نماد پروازهای دوربرد مسافری."
+  },
+  {
+    id: 3,
+    type: "missile",
+    title: "فالکون ۹",
+    year: 2010,
+    cover: "assets/img/khabar.jpg",
+    summary: "موشک فضایی قابل استفاده مجدد برای کاهش هزینه پرتاب."
+  }
+];
